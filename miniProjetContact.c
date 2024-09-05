0326bde08ad2c4fb143a0ae1e8d5befdcc6a75fb
unknown <abdeljabbarmoudiri17@gmail.com>
2024-09-05 16:01:58 +0100

    duxieme commit

---
+++ miniProjetContact.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+int count=0;
+typedef struct {
+    char nom[100],numero[100],adresse[100],email[100];
+}Contact;
+Contact con[100];
+int main()
+{
+
+    menuPrincipal();
+    return 0;
+}
+void menuPrincipal();
+void menuPrincipal() {
+int choix;
+    printf("Choisissez un chiffre :\n");
+    printf("1. Ajouter .\n");
+    printf("2. Afficher .\n");
+    printf("3. Rechercher . \n");
+    printf("4. modifier. \n");
+    printf("5. Supprimer . \n");
+    printf("6. Quitter\n");
+    scanf("%d", &choix);
+     switch(choix) {
+        case 1 :    printf("Saisir le nom du contact : ");
+                    scanf(" %[^\n]s",&con[count].nom);
+                    printf("Saisir numero du contact : ");
+                    scanf(" %[^\n]s", &con[count].numero);
+                    printf("Saisir le adress du contact : ");
+                    scanf(" %[^\n]s", &con[count].adresse);
+                    printf("Saisir le email du contact : ");
+                    scanf(" %[^\n]s", &con[count].email);
+                    count++;
+                    menuPrincipal();
+                    break;
+
+        case 2 :
+            for(int i=0;i<count;i++)
+            {printf("%s |  %s |  %s | %s \n", con[i].nom,con[i].numero,con[i].adresse,con[i].email);}
+                    menuPrincipal();
+
+        case 3 : break;
+        case 4 :  break;
+        case 5 :  break;
+        case 6 : exit;
+    }
+}
